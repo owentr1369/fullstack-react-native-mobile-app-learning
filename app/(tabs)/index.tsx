2712d490fc6bace0ff4d2e6581e65814b2863644
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-11-01 20:03:25 +0700

    Add navigation links to HomeScreen
    - Introduced links to the Onboarding and Avengers pages in the HomeScreen component.
    - Enhanced user navigation experience within the app.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white text-blue-500">
       <Text className="text-blue-500">Welcome to Nativewind!</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avengers">Avengers</Link>
     </View>
   );
 }
